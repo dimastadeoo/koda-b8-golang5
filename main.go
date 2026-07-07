3ac6c12ead6fccb8ffaa360421c2ce2133be231c
dimastadeoo <dimastadeoo@gmail.com>
2026-07-07 17:35:06 +0700

    fix: improve user feedback and screen management in user menu and main flow

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func userMenu(s *auth.AuthService, u *auth.Users){
 	LoopUserMenu:
 	for {
+		auth.ClearScreen()
 		fmt.Println("------------------------------------------------------")
 		fmt.Println("1. List User \n2. Ganti Password \n3. Logout")
 		choice := auth.ReadString("Pilih: ")
@@ -18,18 +19,24 @@ func userMenu(s *auth.AuthService, u *auth.Users){
 			for _, Alluser := range s.DB{
 				fmt.Printf("- %-15s | %-20s\n", Alluser.Username, Alluser.Fullname)
 			}
+			auth.WaitForKey("Tekan Enter untuk Kembali ke menu user....")
 		case "2":
 			op := auth.ReadString("Masukkan Password Lama: ")
 			np := auth.ReadString("Masukkan Password Baru: ")
 			cp := auth.ReadString("Konfirmasi Password Baru: ")
 			if err := s.ChangePasswd(u.Username, op, np, cp); err != nil{
 				fmt.Println(err)
+				choice = "2"
+				auth.WaitForKey("Tekan Enter untuk coba lagi....")
+			}else{
+				auth.WaitForKey("Tekan Enter untuk Kembali ke menu user....")
 			}
 		case "3":
 			auth.WaitForKey("Tekan Enter untuk Logout dan kembali ke menu awal....")
 			break LoopUserMenu
 		default: 
 			fmt.Println("Pilih hanya angka 1 - 3")
+			auth.WaitForKey("Tekan Enter untuk coba lagi....")
 			continue
 		}	
 	}
@@ -45,6 +52,7 @@ func main(){
 	}()
 
 	for {
+		auth.ClearScreen()
 		fmt.Println("-------Selamat Datang---------")
 		fmt.Println("1. Register\n2. Login\n3. Exit")
 		choice := auth.ReadString("Pilih: ")
@@ -63,13 +71,16 @@ func main(){
 			user, err := service.Login(un, pw)
 			if err != nil{
 				fmt.Println(err)
+				auth.WaitForKey("Tekan Enter untuk coba lagi...")
 			}else{
+				auth.WaitForKey("Tekan Enter untuk masuk ke menu user....")
 				userMenu(service, user)
 			}
 		case "3":
 			os.Exit(0)
 		default: 
 			fmt.Println("Pilih hanya angka 1 - 3")
+			auth.WaitForKey("Tekan Enter untuk coba lagi....")
 			continue
 		}
 	}
