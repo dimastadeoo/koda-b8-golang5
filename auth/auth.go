57fa4b58df703cbe1fac1b5b758df54ec20b89c9
dimastadeoo <dimastadeoo@gmail.com>
2026-07-07 11:26:40 +0700

    feat: implement authentication service with user login

--- auth/auth.go
+++ auth/auth.go
@@ -1,10 +1,10 @@
 package auth
 
 import (
-	"fmt"
-	"errors"
 	"crypto/md5"
 	"encoding/hex"
+	"errors"
+	"fmt"
 )
 
 type Users struct {
@@ -19,10 +19,12 @@ type AuthService struct {
 
 // fungsi hash passwd jadi md5
 func hashPasswd (password string) string{
-	hash := md5.Sum([]byte(password))
-	return hex.EncodeToString(hash[:])
+	hash := md5.New()
+	hash.Write([]byte(password))
+	return hex.EncodeToString(hash.Sum(nil))
 }
 
+// method register
 func (s *AuthService) Register(user string, passwd string){
 	if _, checkData := s.DB[user]; checkData{
 		panic("User sudah terdaftar")
@@ -31,6 +33,18 @@ func (s *AuthService) Register(user string, passwd string){
 	hashPwd := hashPasswd(passwd) //input passwd kita hash dulu, baru masukkan ke data user
 	s.DB[user] = &Users{Username: user, Password: hashPwd}
 	fmt.Println("Registrasi sukses!")
-
 }
 
+// method login
+func (s *AuthService) Login(user string, passwd string) error {
+	username, checkData := s.DB[user]
+	if !checkData {
+		return errors.New("Username tidak ditemukan")
+	}
+
+	if username.Password != hashPasswd(passwd){
+		return errors.New("Password tidak sesuai")
+	}
+	fmt.Printf("Selamat Datang %s!\n", user)
+	return nil
+}
