7fe21bc7748b2c7f386f4cf3e20048281befb32c
dimastadeoo <dimastadeoo@gmail.com>
2026-07-07 17:34:59 +0700

    feat: add ClearScreen function for improved console output management

--- auth/input.go
+++ auth/input.go
@@ -4,8 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
 	"strings"
 )
+
+func ClearScreen(){
+	switch runtime.GOOS {
+		case "windows" :
+			cmd := exec.Command("cmd", "/c", "cls")
+			cmd.Stdout = os.Stdout
+			cmd.Run()
+		default :
+			fmt.Print("\033[H\033[2J\033[3J")
+	}
+}
 
 func ReadString(input string) string{
 	reader := bufio.NewReader(os.Stdin)
